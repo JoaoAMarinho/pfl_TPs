ed4612cc6cf5d5b7b9410b345ff3c23aeac1262f
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2021-11-09 15:06:13 +0000

    Fib ex

--- Fib.hs
+++ Fib.hs
@@ -1,3 +1,15 @@
 -- Fibonacci recursive version
 fibRec :: (Integral a) => a -> a
-fibRec n = 
+fibRec 0 = 0
+fibRec 1 = 1
+fibRec n = fibRec (n-1) + fibRec (n-2)
+
+-- Fibonacci dynamic version
+fibLista :: (Integral a) => a -> a
+fibLista i = (!!) (0 : 1 : [ 10 | v<-[2..i]]) (fromIntegral i)
+
+-- Fibonacci infinite version
+infiniteFibList = 0 : 1 : zipWith (+) infiniteFibList (tail infiniteFibList)
+
+fibListaInfinita :: (Integral a) => a -> a
+fibListaInfinita n = (!!) infiniteFibList (fromInt n)
