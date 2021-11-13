d3d92040c71f0decbf168d174d52709fe1089d34
Margarida Vieira <margaridajcv.27@gmail.com>
2021-11-13 17:08:19 +0000

    initial version of mulBN

--- BigNumber.hs
+++ BigNumber.hs
@@ -13,12 +13,31 @@ output bn | fst bn = reverse numbers
           | not (fst bn) = '-' : reverse numbers
           where numbers = map intToDigit (snd bn)
 
-somaBN, subBN :: BigNumber -> BigNumber -> BigNumber
+somaBN, subBN, mulBN :: BigNumber -> BigNumber -> BigNumber
 
 somaBN bn1 bn2 | fst bn1 == fst bn2 = (fst bn1, auxSoma numbers1 numbers2 0)
                | otherwise = subBN bn1 bn2
                where numbers1 = snd bn1 ++ repeat 0
                      numbers2 = snd bn2 ++ repeat 0
+
+subBN bn1 bn2 | fst bn1 /= fst bn2 = somaBN bn1 (not (fst bn2), snd bn2)
+							| numbers1 == numbers2 = (True, [0])
+							| length numbers1 > length numbers2 = auxSub bn1 bn2
+							| length numbers2 > length numbers1 = auxSub bn2 bn1
+							| maior numbers1 numbers2 = auxSub bn1 bn2
+							| otherwise = auxSub bn2 bn1
+							where numbers1 = snd bn1
+							      numbers2 = snd bn2
+
+mulBN bn1 bn2 | fst bn1 == fst bn2 = (True, auxMul numbers1 numbers2)
+              | otherwise = (False, auxMul numbers1 numbers2)
+              where numbers1 = snd bn1
+                    numbers2 = snd bn2
+
+-- divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
+
+
+-------------------------------- Auxiliar functions --------------------------------
 
 auxSoma :: [Int] -> [Int] -> Int -> [Int]
 auxSoma (0:xs) (0:ys) 0 = []
@@ -28,30 +47,22 @@ auxSoma (x:xs) (y:ys) carry | soma > 9 = [mod soma 10] ++ auxSoma xs ys newCarry
                             where soma = x + y + carry
                                   newCarry = div soma 10
 
-subBN bn1 bn2 | fst bn1 /= fst bn2 = somaBN bn1 (not (fst bn2), snd bn2)
-							| numbers1==numbers2 = (True, [0])
-							| length numbers1 > length numbers2 = auxSub bn1 bn2
-							| length numbers2 > length numbers1 = auxSub bn2 bn1
-							| maior numbers1 numbers2 = auxSub bn1 bn2
-							| otherwise = auxSub bn2 bn1
-							where numbers1 = snd bn1
-							      numbers2 = snd bn2
 
+-- multiplies every number in the BN by the given scalar
+scaleBN :: Int -> [Int] -> [Int]
+scaleBN s [] = []
+scaleBN s (x:xs) = somaBN mul (0 : scaleBN s xs) carry
+                   where mul = mod (s * x) 10
+                         carry = div (s * x) 10
+          
+
+auxMul :: [Int] -> [Int] -> [Int]
+auxMul [] _ = []
+auxMul (x:xs) ys = somaBN (scaleBN x ys) (0: auxMul xs ys) 0
+
+
+-- true if the 1st BN is bigger than the 2nd BN, false otherwise
 maior :: [Int] -> [Int] -> [Int]
 maior [] [] = False
 maior (x:xs) (y:ys) | x>y = True
-							      | otherwise maior xs ys
-
--- mulBN :: BigNumber -> BigNumber -> BigNumber
-
--- divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
-
-
--- multiplica por um escalar
--- auxMult :: Int -> [Int] -> [Int]
--- auxMult s [] = []
--- auxMult s (x:xs) = somaBN (toBN (s*x)) (0 : auxMult s xs)
---
--- multBN :: [Int] -> [Int] -> [Int]
--- multBN [] _ = []
--- multBN (x:xs) ys = somaBN (auxMult x ys) (0: multBN xs ys)
+							      | otherwise = maior xs ys
