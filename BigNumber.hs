17fce225ee2843d7a97160352b63e37a1e3c5b3a
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2021-11-22 16:14:39 +0000

    j

--- BigNumber.hs
+++ BigNumber.hs
@@ -1,56 +1,59 @@
 module BigNumber(BigNumber, somaBN, subBN, mulBN, divBN, safeDivBN) where
 import Data.Char
 
+--BigNumber definition
 type BigNumber = (Bool,[Int])
 
 scanner :: String -> BigNumber
-scanner str | isDigit(head str) = (True, map digitToInt str)
-            | head str == '+' = (True, map digitToInt (tail str))
-            | head str == '-' = (False, map digitToInt (tail str))
-            | otherwise = error "Invalid input"
+scanner str | isDigit(head str) = (True, map digitToInt str)       -- String starts with digit
+            | head str == '+' = (True, map digitToInt (tail str))  -- Strings starts with a '+' (positive number)
+            | head str == '-' = (False, map digitToInt (tail str)) -- String starts with a '-' (negative number)
+            | otherwise = error "Invalid input"                    -- Invalid input
 
 output :: BigNumber -> String
-output bn | fst bn = numbers
-          | otherwise = '-' : numbers
+output bn | fst bn = numbers          -- Positive number
+          | otherwise = '-' : numbers -- Negative number
           where numbers = map intToDigit (snd bn)
 
 somaBN, subBN, mulBN :: BigNumber -> BigNumber -> BigNumber
 
-somaBN bn1 bn2 | fst bn1 == fst bn2 = (fst bn1, reverse (auxSoma numbers1 numbers2 0))
-               | otherwise = subBN bn1 (not (fst bn2), snd bn2)
+somaBN bn1 bn2 | fst bn1 == fst bn2 = (fst bn1, reverse (auxSoma numbers1 numbers2 0)) -- Same signal
+               | otherwise = subBN bn1 (not (fst bn2), snd bn2)                        -- Different signals is basically a subtraction
                where numbers1 = reverse (snd bn1)
                      numbers2 = reverse (snd bn2)
 
-subBN bn1 bn2 | numbers1 == numbers2 && fst bn1 == fst bn2 = (True, [0]) -- symmetric numbers
-              | fst bn1 /= fst bn2 = somaBN bn1 (not (fst bn2), snd bn2)
-              | maior (snd bn1) (snd bn2) = (fst bn1, reverse (auxSub numbers1 numbers2 0))
-              | otherwise = (not (fst bn2), reverse (auxSub numbers2 numbers1 0))
+subBN bn1 bn2 | numbers1 == numbers2 && fst bn1 == fst bn2 = (True, [0])                    -- Symmetric numbers
+              | fst bn1 /= fst bn2 = somaBN bn1 (not (fst bn2), snd bn2)                    -- Different signals is basically a sum
+              | maior (snd bn1) (snd bn2) = (fst bn1, reverse (auxSub numbers1 numbers2 0)) -- Same signal and bn1 greater than bn2
+              | otherwise = (not (fst bn2), reverse (auxSub numbers2 numbers1 0))           -- bn2 greater or equal to bn1
               where numbers1 = reverse (snd bn1)
                     numbers2 = reverse (snd bn2)
 
 
-mulBN bn1 bn2 | numbers1 == [0] || numbers2 == [0] = (True, [0])
-              | fst bn1 == fst bn2 = (True, reverse (auxMul numbers1 numbers2))
-              | otherwise = (False, reverse (auxMul numbers1 numbers2))
+mulBN bn1 bn2 | numbers1 == [0] || numbers2 == [0] = (True, [0])                -- Multiplication by zero
+              | fst bn1 == fst bn2 = (True, reverse (auxMul numbers1 numbers2)) -- Same signal
+              | otherwise = (False, reverse (auxMul numbers1 numbers2))         -- Different signals
               where numbers1 = reverse (snd bn1)
                     numbers2 = reverse (snd bn2)
 
 divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
 
-divBN bn1 (True, [1]) = (bn1, (True, [0]))
-divBN bn1 bn2 | maior numbers1 numbers2 = ((True, fst res),(True, snd res))
-              | otherwise = ((True, [0]), bn1)
+divBN bn1 bn2 | bn2 == (True, [1]) = (bn1, (True, [0]))                     -- Divison by one
+              | bn1 == bn2 = ((True, [1]), (True,[0]))                      -- Divison by itself
+              | maior numbers1 numbers2 = ((True, fst res),(True, snd res)) -- bn1 greater than bn2
+              | otherwise = ((True, [0]), bn1)                              -- bn2 greater than bn1 (quocient is 0 with bn1 as remainder)
                 where numbers1 = (snd bn1)
                       numbers2 = (snd bn2)
                       res = auxDiv numbers1 numbers2 [] []
 
 safeDivBN :: BigNumber -> BigNumber -> Maybe (BigNumber, BigNumber)
-safeDivBN bn1 (_, [0]) = Nothing
-safeDivBN bn1 bn2 =   Just (divBN bn1 bn2)
+safeDivBN bn1 (_, [0]) = Nothing           -- Divison by zero
+safeDivBN bn1 bn2 =   Just (divBN bn1 bn2) -- Divison by a number not zero
 
 
--------------------------------- Auxiliar functions --------------------------------
+-------------------------------- Auxiliary functions --------------------------------
 
+-- Auxiliary function for sum
 auxSoma :: [Int] -> [Int] -> Int -> [Int]
 auxSoma [] [] carry = if carry == 0 then [] else [carry]
 auxSoma (x:xs) [] carry = [mod (x + carry) 10] ++ auxSoma xs [] (div (x + carry) 10)
@@ -60,7 +63,7 @@ auxSoma (x:xs) (y:ys) carry | soma > 9 = [mod soma 10] ++ auxSoma xs ys newCarry
                             where soma = x + y + carry
                                   newCarry = div soma 10
 
-
+-- Auxiliary function for subtraction
 auxSub :: [Int] -> [Int] -> Int -> [Int]
 auxSub [] [] _ = []
 auxSub (x:[]) [] carry = if ((x - carry) == 0) then [] else [x - carry]
@@ -70,38 +73,39 @@ auxSub (x:xs) ys carry | soma > x = [x + 10 - soma] ++ auxSub xs ys_ 1
                        where soma = if (ys == []) then carry else (head ys + carry)
                              ys_ = if (ys == []) then [] else (tail ys)
 
-
--- multiplies every number in the BN by the given scalar
-scaleBN :: Int -> [Int] -> Int -> [Int]
-scaleBN s [] carry = if (carry == 0) then [] else [carry]
-scaleBN s (x:xs) carry = [mul] ++ scaleBN s xs newCarry
-                         where mul = mod (s * x + carry) 10
-                               newCarry = div (s * x + carry) 10
-
+-- Auxiliary function for multiplication
 auxMul :: [Int] -> [Int] -> [Int]
 auxMul xs ys = foldl (\x (y,i) -> auxSoma x ((take i (repeat 0)) ++ y) 0) [] (zip [scaleBN x ys 0 | x <- xs] [0,1..])
 
-
--- true if the first BN is bigger than the second BN, false otherwise
-maior :: [Int] -> [Int] -> Bool
-maior [] [] = False
-maior (x:xs) (y:ys) | length (x:xs) > length (y:ys) = True
-                    | length (y:ys) > length (x:xs) = False
-                    | x > y = True
-                    | y > x = False
-                    | otherwise = maior xs ys
-
+-- Auxiliary function for division
 auxDiv :: [Int] -> [Int] -> [Int] -> [Int] -> ([Int], [Int])
 auxDiv [] _ resto quociente = (dropWhile (==0) quociente, resto)
-auxDiv (x:xs) divisor dividendo q | newDividendo == divisor = auxDiv xs divisor [] (1:q)
+auxDiv (x:xs) divisor dividendo q | newDividendo == divisor = auxDiv xs divisor [] (q++[1])
                                   | maior newDividendo divisor = auxDiv xs divisor (fst divison) (q++[snd divison])
-                                  | otherwise = auxDiv xs divisor newDividendo q
-                                  where newDividendo = dividendo++[x]
+                                  | otherwise = auxDiv xs divisor newDividendo (q++[0])
+                                  where newDividendo =  (dropWhile (==0) dividendo)++[x]
                                         divison = subUntil newDividendo divisor 0
 
+-- Subtract two lists representing a number, returning the remainder and the number of subtracions
 subUntil :: [Int] -> [Int] -> Int -> ([Int], Int)
 subUntil dividendo divisor i | dividendo == divisor = ([0], i+1)
                              | maior dividendo divisor = subUntil (reverse (auxSub revDividendo revDivisor 0)) divisor (i + 1)
                              | otherwise = (dividendo, i)
                              where revDividendo = (reverse dividendo)
                                    revDivisor = (reverse divisor)
+
+-- Multiplies a list representing a number by a scalar
+scaleBN :: Int -> [Int] -> Int -> [Int]
+scaleBN s [] carry = if (carry == 0) then [] else [carry]
+scaleBN s (x:xs) carry = [mul] ++ scaleBN s xs newCarry
+                        where mul = mod (s * x + carry) 10
+                              newCarry = div (s * x + carry) 10
+
+-- Checks if the first list representing a number is bigger than the second
+maior :: [Int] -> [Int] -> Bool
+maior [] [] = False
+maior (x:xs) (y:ys) | length (x:xs) > length (y:ys) = True
+                   | length (y:ys) > length (x:xs) = False
+                   | x > y = True
+                   | y > x = False
+                   | otherwise = maior xs ys
--- Fib.hs
+++ Fib.hs
@@ -1,10 +1,11 @@
 import BigNumber(BigNumber, somaBN, subBN, mulBN, divBN, safeDivBN)
--------------------------------- Fibonacci recursive version --------------------------------
+
+-------------------------------- Fibonacci recursive version ------------------------------
+
 fibRec :: (Integral a) => a -> a
 fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec (n-1) + fibRec (n-2)
-
 
 -------------------------------- Fibonacci dynamic version --------------------------------
 
@@ -12,39 +13,40 @@ fibLista :: (Integral a) => a -> a
 fibLista n = fromIntegral (dynamicFibList !! (fromIntegral n))
            where dynamicFibList = 0:1:[ (dynamicFibList !! fromIntegral(x-1)) + (dynamicFibList !! fromIntegral(x-2)) | x <- [2..n]]
 
-
--------------------------------- Fibonacci infinite version --------------------------------
-infiniteFibList = 0 : 1 : zipWith (+) infiniteFibList (tail infiniteFibList)
+-------------------------------- Fibonacci infinite version -------------------------------
 
 fibListaInfinita :: (Integral a) => a -> a
 fibListaInfinita n = fromIntegral ( infiniteFibList !! (fromIntegral n))
+                   where infiniteFibList = 0 : 1 : zipWith (+) infiniteFibList (tail infiniteFibList)
 
-
------------------------------------ BigNumber Version ------------------------------------
+-------------------------------- Fibonacci recursive version using BigNumber --------------
 
 fibRecBN :: BigNumber -> BigNumber
 fibRecBN (True, [0]) = (True, [0])
 fibRecBN (True, [1]) = (True, [1])
 fibRecBN (True, bn)  = somaBN (fibRecBN (subBN (True, bn) (True, [1])) ) (fibRecBN (subBN (True, bn) (True, [2])) )
 
---
+-------------------------------- Fibonacci dynamic version using BigNumber ----------------
 
 fibListaBN :: BigNumber -> BigNumber
 fibListaBN n = select n dynamicFibListBN
             where dynamicFibListBN = (True, [0]) : (True, [1]) : [ somaBN (select (subBN x (True, [1])) dynamicFibListBN) (select (subBN x (True, [2])) dynamicFibListBN) | x <- rangeBn ]
                   rangeBn = iterateBn (somaBN (True, [1])) (True, [2]) (somaBN n (True, [1]))
 
---
-infiniteFibListBN = (True, [0]) : (True, [1]) : zipWith (somaBN) infiniteFibListBN (tail infiniteFibListBN)
+-------------------------------- Fibonacci infinite version using BigNumber ---------------
 
 fibListaInfinitaBN :: BigNumber -> BigNumber
 fibListaInfinitaBN n = select n infiniteFibListBN
+                     where infiniteFibListBN = (True, [0]) : (True, [1]) : zipWith (somaBN) infiniteFibListBN (tail infiniteFibListBN)
 
--- Aux functions
+-------------------------------- Auxiliary functions --------------------------------------
+
+-- Select a BigNumber from a list with index specified by the first argument
 select :: BigNumber -> [BigNumber] -> BigNumber
 select (True,[0]) xs = head xs
 select bn (x:xs)     = select (subBN bn (True, [1])) xs
 
+-- Apply a function to a BigNumber until a certain limit, returning the results in a list
 iterateBn :: (BigNumber -> BigNumber) -> BigNumber -> BigNumber  -> [BigNumber]
 iterateBn f a limit | a /= limit = a : iterateBn f (f a) limit
                     | otherwise = []
