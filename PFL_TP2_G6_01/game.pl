43c502f902053aa85ff03c5fcf4f0c93739c155e
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-01-10 09:21:17 +0000

    structured turn change

--- PFL_TP2_G6_01/game.pl
+++ PFL_TP2_G6_01/game.pl
@@ -28,18 +28,26 @@ piece_in_board(Board, Type, X, Y):-
 piece_in_board(Board, Type, X, Y).
 
 /*
+* Returns the new turn after the samurais play, according to the current mode: 
+* change_turn(+Mode, -NewTurn).
+*/
+change_turn(pvp,  ninja).
+change_turn(easy, bot).
+change_turn(hard, bot).
+
+/*
 * Game handler:
 * play_game(+playerTurn, +Board, +Player1Points, +Player2Points).
 */
-play_game(_, _, Points1, _):- % check if game has ended
+play_game(_, _, Points1, _, _):- % check if game has ended
     Points1 == 4,
     write('\nSamurais WON!\n').
 
-play_game(_, _, _, Points2):- % check if game has ended
+play_game(_, _, _, Points2, _):- % check if game has ended
     Points2 == 4,
     write('\nNinjas WON!\n').
 
-play_game(samurai, Board, Player1Points, Player2Points):- % play piece according to player turn
+play_game(samurai, Board, Player1Points, Player2Points, Mode):- % play piece according to player turn
     print_board(Board),
     write('\nSamurais turn\n'),
     repeat,
@@ -47,9 +55,10 @@ play_game(samurai, Board, Player1Points, Player2Points):- % play piece according
     piece_in_board(Board, samurai, X, Y),
     valid_piece_move(samurai, Board, X, Y, Nx, Ny),!,
     move_piece(samurai, Board, X, Y, Nx, Ny, NewBoard, Player1Points, NewPlayer1Points),
-    play_game(ninja, NewBoard, NewPlayer1Points, Player2Points).
+    change_turn(Mode, NewTurn),
+    play_game(NewTurn, NewBoard, NewPlayer1Points, Player2Points, Mode).
 
-play_game(ninja, Board, Player1Points, Player2Points):- % play piece according to player turn
+play_game(ninja, Board, Player1Points, Player2Points, Mode):- % play piece according to player turn
     print_board(Board),
     write('\nNinjas turn\n'),
     repeat,
@@ -57,12 +66,20 @@ play_game(ninja, Board, Player1Points, Player2Points):- % play piece according t
     piece_in_board(Board, ninja, X, Y),
     valid_piece_move(ninja, Board, X, Y, Nx, Ny),!,
     move_piece(ninja, Board, X, Y, Nx, Ny, NewBoard, Player2Points, NewPlayer2Points),
-    play_game(samurai, NewBoard, Player1Points, NewPlayer2Points).
+    play_game(samurai, NewBoard, Player1Points, NewPlayer2Points, Mode).
+
+play_game(bot, Board, Player1Points, Player2Points, Mode):- % play piece according to player turn
+    print_board(Board),
+    write('\Bot turn\n'),
+    repeat,
+    read_move(X, Y, Nx, Ny),
+    piece_in_board(Board, ninja, X, Y),
+    valid_piece_move(ninja, Board, X, Y, Nx, Ny),!,
+    move_piece(ninja, Board, X, Y, Nx, Ny, NewBoard, Player2Points, NewPlayer2Points),
+    play_game(samurai, NewBoard, Player1Points, NewPlayer2Points, Mode).
 
 /*
 * Game starter:
-* game(+Type).
+* game(+Mode).
 */
-game(pvp):- build_board(Board), play_game(samurai, Board, 0, 0).
-game(easy):- build_board(Board), play_game(samurai, Board, 0, 0).
-game(hard):- build_board(Board), play_game(samurai, Board, 0, 0).
+game(Mode):- build_board(Board), play_game(samurai, Board, 0, 0, Mode).
